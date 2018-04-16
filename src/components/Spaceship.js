e790aba26fa1dc02511d7d577f0448ed2be08a8c
Alexander Spanos <alexisspa9@gmail.com>
2018-04-16 19:46:17 +0000

    automatically backed up by learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -1,1 +1,12 @@
 // Code The Spaceship Component Here
+import React from 'react'
+
+class Spaceship extends React.Component {
+  render() {
+    return (
+      
+    )
+  }
+}
+
+export default Spaceship;
